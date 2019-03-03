43f51f17389dad010c28328c829798f4d5dd95f6
André <andre.tiago.at@gmail.com>
2019-03-03 11:27:47 +0000

    void xml atualizado
    Estava a assumir que <model file="..." /> estava dentro de <group><models> mas isso só acontece a partir da Fase 2

--- Engine/parser.cpp
+++ Engine/parser.cpp
@@ -11,38 +11,12 @@ void xml(std::string xml_name) {
     pugi::xml_parse_result result = doc.load_file(xml_name.c_str());
 
     if (result) {
-        /** <scene> **/
         pugi::xml_node scene = doc.child("scene");
 
         for (pugi::xml_node scene_child = scene.first_child(); scene_child; scene_child = scene_child.next_sibling()) {
-            /** <group> **/
-            if (scene_child.name() == std::string("group")) {
-                group_xml(scene_child);
+            if (scene_child.name() == std::string("model")) {
+                files_list.emplace_back(scene_child.attribute("file").value());
             }
         }
     } else perror("Unable to open file");
-}
-
-void group_xml(pugi::xml_node group) {
-    bool flag_models = false;
-
-    for (pugi::xml_node group_child = group.first_child(); group_child; group_child = group_child.next_sibling()) {
-
-        /** <group> **/
-        if (group_child.name() == std::string("group")) {
-            group_xml(group_child);
-        }
-
-        /** <models> **/
-        if (group_child.name() == std::string("models") && !flag_models) {
-            flag_models = true;
-            for (pugi::xml_node models_child = group_child.first_child(); models_child; models_child = models_child.next_sibling()) {
-
-                /** <model/> **/
-                if (models_child.name() == std::string("model")) {
-                    files_list.emplace_back(models_child.attribute("file").value());
-                }
-            }
-        }
-    }
 }
